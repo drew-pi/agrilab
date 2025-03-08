c91dbab3591f2511bf9c7bfa003d9440cc002f58
Andrew Piatetsky <apiatetsky30@stuy.edu>
2025-03-08 15:52:15 -0500

    no need for docker file basically running native container

--- containers/scripts/start.sh
+++ containers/scripts/start.sh
@@ -3,17 +3,14 @@
 # exit if error occurs
 set -e
 
-DOCKERFILE="../Dockerfile"
-CONTAINER="jetson_jupyter"
-
-# build the container
-./build.sh $CONTAINER $DOCKERFILE
+NAME=ml_lab
+IMAGE="nvcr.io/nvidia/l4t-ml:r32.7.1-py3"
 
 # check to see if already existing version
-if [ "$(sudo docker ps -aq -f name=$CONTAINER)" ]; then
+if [ "$(sudo docker ps -aq -f name=$NAME)" ]; then
     echo "Removing existing container..."
-    sudo docker stop $CONTAINER
-    sudo docker rm $CONTAINER
+    sudo docker stop $NAME
+    sudo docker rm $NAME
 fi
 
 if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
@@ -23,9 +20,9 @@ else
 fi
 
 echo "Starting new container..."
-sudo docker run -d --name $CONTAINER --network=host --runtime=nvidia \
+sudo docker run -it -d --name $NAME --network=host --runtime=nvidia \
     -v $ROOT/notebooks:/root/notebooks \
-    $CONTAINER
+    $IMAGE
 
 echo "Container started"
-echo $(sudo docker logs --tail=2 jetson_jupyter)
+echo $(sudo docker logs --tail=2 $NAME)
