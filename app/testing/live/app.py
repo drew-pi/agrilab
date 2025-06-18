f797e04b1784e0d6767addfddda0cba9b19c0241
Andrew Piatetsky <apiatetsky30@stuy.edu>
2025-06-18 13:30:15 -0400

    small tweaks

--- app/testing/live/app.py
+++ app/testing/live/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__, template_folder='/templates')
 
 @app.route("/")
 def index():
-    return render_template("viewer.html", hls_url="http://localhost:8080/hls/stream.m3u8")
+    JETSON_IP=os.getenv("JETSON_IP")
+    return render_template("viewer.html", hls_url=f"http://{JETSON_IP}:8080/hls/stream.m3u8")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
