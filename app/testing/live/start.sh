aea707acefb3f3fbe64ff9b9e3a1e68ca0a9dc15
Andrew Piatetsky <apiatetsky30@stuy.edu>
2025-06-18 13:38:59 -0400

    more improvement so it actually works

--- app/testing/live/start.sh
+++ app/testing/live/start.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-CONTAINER_NAME=live
+nginx
 
-sudo docker build -t $CONTAINER_NAME .
-
-sudo docker run --rm --name live-stack --network host \
-    -v $(pwd)/hls:/var/www/hls \
-    $CONTAINER_NAME
+python3 /app.py
