776823c34006624fc4ec56fd0ee5c0e7a9fee396
Andrew Piatetsky <apiatetsky30@stuy.edu>
2025-06-18 14:36:52 -0400

    added latency helpers

--- app/testing/live/run.sh
+++ app/testing/live/run.sh
@@ -2,10 +2,28 @@
 
 set -e
 
-CONTAINER_NAME=live
+CONTAINER_NAME=live-poc
 
 sudo docker build -t $CONTAINER_NAME .
 
-sudo docker run --rm --name live-stack --network host \
+# start in detached mode
+sudo docker run --rm -d --name live --network host \
     -v $(pwd)/hls:/var/www/hls \
     $CONTAINER_NAME
+
+# run camera capture
+ffmpeg -re -f v4l2 -fflags +discardcorrupt \
+       -input_format mjpeg -video_size 640x480 -framerate 30 \
+       -i /dev/video0 \
+       -vf format=yuv420p \
+       -c:v libx264 -preset ultrafast -tune zerolatency \
+       -f flv rtmp://10.49.26.98/live/stream
+
+ffmpeg -re -f v4l2 -fflags +discardcorrupt \
+    -input_format mjpeg -video_size 1280x960 -framerate 5 \
+    -i /dev/video0 \
+    -vf format=yuv420p \
+    -c:v libx264 -preset ultrafast -tune zerolatency \
+    -g 1 -keyint_min 1 -sc_threshold 0 \
+    -f flv rtmp://10.49.26.98/live/stream \
+    -loglevel warning # for production so that doesn't cloud the screen
